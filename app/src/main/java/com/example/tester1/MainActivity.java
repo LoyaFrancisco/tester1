f93ca61d7ab298b127cae058ca1f02d7860a904a
LoyaFrancisco <Loyaa11@gmail.com>
2020-02-24 19:47:35 -0800

    added x feature
    x feature does this
    - ...
    -
    -

--- app/src/main/java/com/example/tester1/MainActivity.java
+++ app/src/main/java/com/example/tester1/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Log.i(TAG, "New feature");
         Log.i(TAG, "scaryFunction: BOOOOOOOO!!!!!!");
     }
 
