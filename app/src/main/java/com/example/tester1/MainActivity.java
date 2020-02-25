5b14edff267ecb527ed60d922b29b6dca9f755c3
michaellofton <28902859+michaellofton@users.noreply.github.com>
2020-02-24 19:18:45 -0800

    added log statement in main

--- app/src/main/java/com/example/tester1/MainActivity.java
+++ app/src/main/java/com/example/tester1/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.tester1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "scaryFunction: BOOOOOOOO!!!!!!");
     }
+
 }
