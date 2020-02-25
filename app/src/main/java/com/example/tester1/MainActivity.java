b00fb6a7a1b6eb8b04de7cfe27f159defb09b2b4
LoyaFrancisco <Loyaa11@gmail.com>
2020-02-24 19:18:39 -0800

    added an initialized button

--- app/src/main/java/com/example/tester1/MainActivity.java
+++ app/src/main/java/com/example/tester1/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.tester1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
